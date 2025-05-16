8087639c0655bf840fb3c738bf3768b2051eda00
Your Name <you@example.com>
2025-05-16 13:52:08 +0900

    add scroll & change textColor

--- android/app/src/main/java/com/square/aircommand/screens/UserGestureFragment.kt
+++ android/app/src/main/java/com/square/aircommand/screens/UserGestureFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.square.aircommand.R
@@ -20,7 +21,7 @@ class UserGestureFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): androidx.constraintlayout.widget.ConstraintLayout {
+    ): View {
         _binding = FragmentUserGestureBinding.inflate(inflater, container, false)
         return binding.root
     }
